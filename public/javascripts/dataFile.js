814bffc7aa979c71c9fda512ee63b947bed2fe09
madibaT <taetsomj@gmail.com>
2018-08-14 13:07:49 +0200

    contributions graph ploted

--- public/javascripts/dataFile.js
+++ public/javascripts/dataFile.js
@@ -1,3 +1,4 @@
+var userInfo = ''
 const getReleases = async () => {
   try {
     await fetch('http://127.0.0.1:3000/javascripts/data.json')
@@ -14,7 +15,7 @@ const getReleases = async () => {
 const dateOfRelease = () => {
   getReleases().then((res) => {
     const releases = res.releases
-    console.log(releases)
+    // console.log(releases)
     const releaseInfo = getReleaseDates(releases)
     var day = 1000 * 60 * 60 * 24 // this gives a day in milliseconds
     var testData = cleanData(releaseInfo)
@@ -33,7 +34,7 @@ const dateOfRelease = () => {
         .tickFormat( //
           {format: d3.time.format('%Y-%m-%d'),
             tickTime: d3.time.day,
-            tickInterval: SprintLength,
+            tickInterval: 1, // SprintLength,
             tickSize: 6})
         .margin({left: 70, right: 30, top: 0, bottom: 0})
         .click(function (d, i, datum) {
@@ -41,7 +42,7 @@ const dateOfRelease = () => {
           developerContributions()
         })
 
-      console.log(releaseInfo.releaseInfo.actualreleaseDates[0])
+      // console.log(releaseInfo.releaseInfo.actualreleaseDates[0])
       var svg = d3.select('#timeline1').append('svg').attr('width', 1000).datum(testData).call(chart)
     }
     //  svg.selectAll(".bar").on("click", function(d){location.replace(d.letter+".html");});
@@ -75,14 +76,13 @@ function getReleaseDates (releases) {
       // console.log('releas day' + releaseInfo.releaseDay)
     }
     if (i === 0 && releaseInfo.daysElapsed !== 0) {
-      var startDate = releaseInfo.actualreleaseDates[0] - releaseInfo.daysElapsed
-      console.log(releaseInfo.actualreleaseDates);
+      var startDate = releaseInfo.actualreleaseDates[0] - releaseInfo.daysElapsed;
+
       (releaseInfo.actualreleaseDates).unshift(startDate)
-      console.log(releaseInfo.actualreleaseDates)
     }
     j++
   }
-  // console.log(releaseInfo.releaser)
+
   return {releaseInfo}
 }
 
@@ -141,21 +141,22 @@ function releaseDetais (releaseInfo, i) {
   var SprintLength = (releaseInfo.daysElapsed) / day
 
   const actualDates = releaseInfo.actualreleaseDates
-  const expectedDates = releaseInfo.expreleaseDates
   // convert to integers for comparison
-  expectedDates.forEach(parseInt)
   actualDates.forEach(parseInt)
+  var clickedSprintLength = (actualDates[i + 1] - actualDates[i]) / day
 
+  // console.log(i)
+  document.getElementById('clickedBar').innerHTML = 'Clicked Bar:   ' + releaseInfo.releaseTags[i]
   document.getElementById('releaser').innerHTML = 'Released by:   ' + releaser
   document.getElementById('date').innerHTML = 'Released on:   ' + releaseDate
   document.getElementById('SprintLength').innerHTML = 'average Sprint Length(days):   ' + SprintLength
-  document.getElementById('SprintLength').innerHTML = 'Current Sprint Length:   ' + SprintLength
+  document.getElementById('clickedSprintLength').innerHTML = 'Clicked Sprint Length:   ' + clickedSprintLength
 }
 
 const contributions = async () => {
   const res = await fetch(`https://api.github.com/repos/${userInfo.organisation}/${userInfo.repository}/stats/contributors?access_token=${userInfo.accessToken}`)
   const contributions = await res.json()
-  console.log(contributions)
+  // console.log(contributions)
   return {contributions}
 }
 
@@ -164,70 +165,38 @@ const developerContributions = () => {
   var contributorCommits = []
   var addPerWeek = []
   var weeks = []
+
   var data = []
   var conData = []
   contributions().then((res) => {
     const contributors = res.contributions
 
-    console.log(contributors)
-    console.log(contributors.length)
     var totalAdditions = []
-    for (var i = 0; i < (contributors).length; i++) {
-      conName[i] = contributors[i].author.login
-      contributorCommits[i] = contributors[i].total
-      for (var j = 0; j < (contributors[i].weeks).length; j++) {
-        if ((contributors[i].weeks[j].a) !== 0) {
-          const week = convertTimestamp(contributors[i].weeks[j].w)
-          data[j] = {
-            'Name': conName[i],
-            'Week': week,
-            'Aditions_per_Week': contributors[i].weeks[j].a
-          }
+
+    for (var i = 0; i < ((contributors[0]).weeks).length; i++) {
+      var k = 0 // this variable will be used to track number of contributors down there
+      var obj = {}
+      for (var j = 0; j < contributors.length; j++) {
+        obj['year'] = convertTimestamp(((contributors[j]).weeks[i]).w)
+        obj[contributors[j].author.login] = ((contributors[j]).weeks[i]).a
+        // getting the list of all contributors
+        if (i === 0) {
+          conName[j] = contributors[j].author.login
+        }
+        // s}
+      }
+
+      for (var n = 0; n < conName.length; n++) {
+        if (obj[conName[n]] === 0) {
+          k++
         }
       }
-      conData[i] = JSON.parse(JSON.stringify(data))
+      if (k !== conName.length) {
+        data[i] = obj
+      }
     }
-    genContributorTable(data)
-    console.log((conData))
+    plotBar(data, conName)
   })
-}
-
-function genContributorTable (data) {
-  function tabulate (data, columns) {
-    d3.select('table').remove()
-    var table = d3.select('#summary').append('table')
-    var thead = table.append('thead')
-    var tbody = table.append('tbody')
-
-    // append the header row
-    thead.append('tr')
-      .selectAll('th')
-      .data(columns).enter()
-      .append('th')
-      .text(function (column) { return column })
-
-      // create a row for each object in the data
-    var rows = tbody.selectAll('tr')
-      .data(data)
-      .enter()
-      .append('tr')
-
-      // create a cell in each row for each column
-    var cells = rows.selectAll('td')
-      .data(function (row) {
-        return columns.map(function (column) {
-          return {column: column, value: row[column]}
-        })
-      })
-      .enter()
-      .append('td')
-      .text(function (d) { return d.value })
-
-    return table
-  }
-
-  // render the tables
-  tabulate(data, ['Name', 'Week', 'Aditions_per_Week'])
 }
 
 // unix timestamp to current date
@@ -251,9 +220,164 @@ function convertTimestamp (timestamp) {
   } else if (hh == 0) {
     h = 12
   }
-
   // ie: 2013-02-18, 8:35 AM
-  time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm
+  time = yyyy + '-' + mm + '-' + dd
 
   return time
+}
+
+// Ploting A Bar graph
+/********************************************************************************/
+// Setup svg using Bostock's margin convention
+
+function plotBar (data, names) {
+  var margin = {top: 80, right: 160, bottom: 100, left: 70}
+  var width = 700 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom
+
+  var svg = d3.select('#barGraph')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+    /* Data in strings like it would be if imported from a csv */
+
+  var parse = d3.time.format('%Y-%m-%d').parse
+  // Transpose the data into layers
+  console.log(data[0].year)
+
+  var dataset = d3.layout.stack()(names.map(function (developer) {
+    return data.map(function (d) {
+      return {x: parse(d.year), y: +d[developer]}
+    })
+  }))
+
+  // Set x, y and colors
+  var x = d3.scale.ordinal()
+    .domain(dataset[0].map(function (d) { return d.x }))
+    .rangeRoundBands([10, width - 10], 0.02)
+
+  var y = d3.scale.linear()
+    .domain([0, d3.max(dataset, function (d) { return d3.max(d, function (d) { return d.y0 + d.y }) })])
+    .range([height, 0])
+
+  var colors = colorFunction()
+
+  // var colors = ['b33040', '#d25c4d', '#f2b447', '#d9d574']
+
+  // Define and draw axes
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient('left')
+    .ticks(10)
+    .tickSize(-width, 0, 0)
+    .tickFormat(function (d) { return d })
+
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom')
+    .tickFormat(d3.time.format('%Y-%m-%d'))
+
+  svg.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis)
+
+  svg.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis)
+
+  // Create groups for each series, rects for each segment
+  var groups = svg.selectAll('g.cost')
+    .data(dataset)
+    .enter().append('g')
+    .attr('class', 'cost')
+    .style('fill', function (d, i) { return colors[i] })
+
+  var rect = groups.selectAll('rect')
+    .data(function (d) { return d })
+    .enter()
+    .append('rect')
+    .attr('x', function (d) { return x(d.x) })
+    .attr('y', function (d) {
+      return y(d.y0 + d.y)
+      console.log(d.y0); console.log(d.y)
+    })
+    .attr('height', function (d) { return y(d.y0) - y(d.y0 + d.y) })
+    .attr('width', x.rangeBand())
+    .on('mouseover', function () { tooltip.style('display', null) })
+    .on('mouseout', function () { tooltip.style('display', 'none') })
+    .on('mousemove', function (d) {
+      var xPosition = d3.mouse(this)[0] - 15
+      var yPosition = d3.mouse(this)[1] - 25
+      tooltip.attr('transform', 'translate(' + xPosition + ',' + yPosition + ')')
+      tooltip.select('text').text(d.y)
+    })
+
+  // Draw legend
+  var legend = svg.selectAll('.legend')
+    .data(colors)
+    .enter().append('g')
+    .attr('class', 'legend')
+    .attr('transform', function (d, i) { return 'translate(30,' + i * 19 + ')' })
+
+  legend.append('rect')
+    .attr('x', width - 18)
+    .attr('width', 18)
+    .attr('height', 18)
+    .style('fill', function (d, i) { return colors.slice().reverse()[i] })
+
+  legend.append('text')
+    .attr('x', width + 5)
+    .attr('y', 9)
+    .attr('dy', '.35em')
+    .style('text-anchor', 'start')
+    .text(function (d, i) {
+      return names[i]
+    })
+
+  // Prep the tooltip bits, initial display is hidden
+  var tooltip = svg.append('g')
+    .attr('class', 'tooltip')
+    .style('display', 'none')
+
+  tooltip.append('rect')
+    .attr('width', 30)
+    .attr('height', 20)
+    .attr('fill', 'white')
+    .style('opacity', 0.5)
+
+  tooltip.append('text')
+    .attr('x', 15)
+    .attr('dy', '1.2em')
+    .style('text-anchor', 'middle')
+    .attr('font-size', '12px')
+    .attr('font-weight', 'bold')
+
+  svg.append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 0 - margin.left)
+    .attr('x', 0 - (height / 2))
+    .attr('dy', '1em')
+    .style('text-anchor', 'middle')
+    .text('Number of lines added')
+    // text label for the x axis
+  svg.append('text')
+    .attr('transform', 'translate(' + (width / 2) + ' ,' + (height + margin.top + 20) + ')')
+    .style('text-anchor', 'middle')
+    .text('Release Dates')
+  svg.append('text')
+    .attr('x', (width / 2))
+    .attr('y', 0 - (margin.top / 2))
+    .attr('text-anchor', 'middle')
+    .style('font-size', '20px')
+    .style('text-decoration', 'underline')
+    .text('Number of line of code added Vs release dates')
+}
+
+function colorFunction () {
+  var colorArray = ['#FF6633', '#FFB399', '#6666FF', '#FF33FF']
+  return colorArray
 }
